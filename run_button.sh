832d2a3a7453401eeaace0028a69fc994f8ebe4d
Jared Zutt <645681@aacps.org>
2020-10-16 12:42:18 +0000

    Complete the Terrapin Card assignment.

--- run_button.sh
+++ run_button.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 cd src
 cd main
 cd java
--- src/main/java/TerrapinCard.java
+++ src/main/java/TerrapinCard.java
@@ -1,4 +1,21 @@
 public class TerrapinCard {
-
-
+  private double balance;
+  public TerrapinCard() {
+    balance = 0;
+  }
+  public TerrapinCard(double balance) {
+    this.balance = balance;
+  }
+  public void payEconomical() {
+    balance -= 2.5 <= balance ? 2.5 : 0;
+  }
+  public void payGourmet() {
+    balance -= 4 <= balance ? 4 : 0;
+  }
+  public void loadMoney(int amount) {
+    balance += amount < 0 ? 0 : amount + balance > 150 ? 150 - balance : amount;
+  }
+  public String toString() {
+    return "The card has " + balance + " dollars";
+  }
 }
